37fca852911314825f87551470f9f4ceb0877fef
Dr. Asis Hallab <asis.hallab@gmail.com>
2021-11-25 10:03:45 -0600

    Added scafold of utils

---
+++ utils.js
@@ -0,0 +1,17 @@
+const processProperties = function(props) {
+
+}
+
+const processAssociation = function(assocProp) {
+
+}
+
+const processScalar = function(scalarProp) {
+
+}
+
+module.exports = {
+  processProperties,
+  processAssociation,
+  processScalar
+}

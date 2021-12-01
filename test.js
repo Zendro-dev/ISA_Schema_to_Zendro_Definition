e25ff38f15a16831a285d917c28ae247ce7a577a
Terazus <batistadominique@hotmail.com>
2021-12-01 07:18:19 -0800

    made output local

--- test.js
+++ test.js
@@ -5,8 +5,8 @@ fs.readdir("./isa_1_0_core/", (err, files) => {
     let output = {}
     files.forEach(file => {
         console.log(`PROCESSING FILE ${file}`)
-        let schemaName = u.getSchemaName(file)
-        let i = require(`./isa_1_0_core/${file}`)
+        const schemaName = u.getSchemaName(file),
+            i = require(`./isa_1_0_core/${file}`)
         output[schemaName] = u.processProperties(i.properties, schemaName)
     });
     // we now need to process reverse relationships from u.relationMapping

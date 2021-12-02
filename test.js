544e07f2562785e7c28f626ce73a9f1e4bac1d76
coeit <cbeiteneuer@gmail.com>
2021-12-02 16:37:33 +0100

    wip: processAssociations
    Co-authored-by: Dominique <batistadominique@hotmail.com>
    Co-authored-by: Asis Hallab <asishallab@users.noreply.github.com>

--- test.js
+++ test.js
@@ -1,13 +1,14 @@
-const fs = require('fs')
-const u = require('./utils.js')
+const fs = require("fs");
+const u = require("./utils.js");
 
 fs.readdir("./isa_1_0_core/", (err, files) => {
-    let output = {}
-    files.forEach(file => {
-        console.log(`PROCESSING FILE ${file}`)
-        const schemaName = u.getSchemaName(file),
-            i = require(`./isa_1_0_core/${file}`)
-        output[schemaName] = u.processProperties(i.properties, schemaName)
-    });
-    // we now need to process reverse relationships from u.relationMapping
+  let output = {};
+  files.forEach((file) => {
+    console.log(`PROCESSING FILE ${file}`);
+    const schemaName = u.getSchemaName(file),
+      i = require(`./isa_1_0_core/${file}`);
+    output[schemaName] = u.processProperties(i.properties, schemaName);
+  });
+  console.log(u.relationMapping);
+  // we now need to process reverse relationships from u.relationMapping
 });
--- utils.js
+++ utils.js
@@ -1,95 +1,115 @@
 let PARENT;
 let relationMapping = {};
 const mapIsaTypeToZendroType = {
-  "string": "String",
+  string: "String",
   "date-time": "DateTime",
-  "number": "Float",
-  "email": "String",
-  "uri": "String"
-}
+  number: "Float",
+  email: "String",
+  uri: "String",
+};
 
-const processProperties = function(propertiesObj, parentName) {
+const processProperties = function (propertiesObj, parentName) {
   PARENT = getSchemaName(parentName);
   const props = Object.entries(propertiesObj);
-  let zendroAttributes = { model: PARENT, "storageType": "sql", attributes: {}, associations: {} }
-  props.forEach(prop => {
+  let zendroAttributes = {
+    model: PARENT,
+    storageType: "sql",
+    attributes: {},
+    associations: {},
+  };
+  props.forEach((prop) => {
     const propDef = prop[1];
     if (propDef.type === "array") {
       const def = processArray(prop);
-      if (def[1] === 'objects') Object.assign(zendroAttributes.associations, def[0])
-      else Object.assign(zendroAttributes.attributes, def[0])
-    }
-    else if (!propDef.type || propDef.type === "object") {
-      Object.assign(zendroAttributes.associations, processAssociation(prop))
-    }
-    else Object.assign(zendroAttributes.attributes, processScalar(prop))
+      if (def[1] === "objects") {
+        Object.assign(zendroAttributes.associations, def[0]);
+      } else Object.assign(zendroAttributes.attributes, def[0]);
+    } else if (!propDef.type || propDef.type === "object") {
+      let [associations, foreignKeys] = processAssociation(prop);
+      Object.assign(zendroAttributes.associations, associations);
+      Object.assign(zendroAttributes.attributes, { [foreignKeys]: "String" });
+    } else Object.assign(zendroAttributes.attributes, processScalar(prop));
   });
 
   // Manually add an 'id' field:
   zendroAttributes.attributes["id"] = "String";
-  console.log('--- ' + PARENT + ' ---')
+  console.log("--- " + PARENT + " ---");
   console.log(zendroAttributes);
   return zendroAttributes;
-}
+};
 
-const recognizeIsaType = function(propDef) {
-  return !propDef.format ? mapIsaTypeToZendroType[propDef.type] : mapIsaTypeToZendroType[propDef.format]
-}
+const recognizeIsaType = function (propDef) {
+  return !propDef.format
+    ? mapIsaTypeToZendroType[propDef.type]
+    : mapIsaTypeToZendroType[propDef.format];
+};
 
-const processArray = function(arrayProp) {
-  console.log(`Processing array ${JSON.stringify(arrayProp[0])}`)
-  const items = arrayProp[1].items
-  if (items['$ref'] || (items.types && items.types === 'object') || (items['anyOf'])) {
-    console.log(`Found multiple references ${items['$ref']}`)
-    return [processAssociation(arrayProp), 'objects']
+const processArray = function (arrayProp) {
+  console.log(`Processing array ${JSON.stringify(arrayProp[0])}`);
+  const items = arrayProp[1].items;
+  if (
+    items["$ref"] ||
+    (items.types && items.types === "object") ||
+    items["anyOf"]
+  ) {
+    console.log(`Found multiple references ${items["$ref"]}`);
+    return [processAssociation(arrayProp), "objects"];
   } else {
     // This is a scalar
-    console.log(`Found multiple scalars ${items.type}`)
-    return [`[${recognizeIsaType(arrayProp)}]`, 'scalars']
+    console.log(`Found multiple scalars ${items.type}`);
+    return [`[${recognizeIsaType(arrayProp)}]`, "scalars"];
   }
-}
+};
 
-const processAssociation = function(assocProp) {
-  console.log(`Processing association ${JSON.stringify(assocProp[0])}`)
-  let to_many = 'items' in assocProp[1],
-      references = 'items' in assocProp[1] ? assocProp[1].items : assocProp[1],
-      schemaName = "";
-  if (Object.keys(references).includes('$ref')) {
+const processAssociation = function (assocProp) {
+  console.log(`Processing association ${JSON.stringify(assocProp[0])}`);
+  const to_many = "items" in assocProp[1],
+    references = "items" in assocProp[1] ? assocProp[1].items : assocProp[1],
+    schemaName = "";
+  if (Object.keys(references).includes("$ref")) {
     // there is a single reference to another schema
-    schemaName = getSchemaName(references['$ref'])
-    schemaName in relationMapping ? relationMapping[schemaName].push(PARENT) : relationMapping[schemaName] = [PARENT]
-  }
-  else if (Object.keys(references).includes('anyOf')) {
+    schemaName = getSchemaName(references["$ref"]);
+    schemaName in relationMapping
+      ? relationMapping[schemaName].push(PARENT)
+      : (relationMapping[schemaName] = [PARENT]);
+  } else if (Object.keys(references).includes("anyOf")) {
     // there are multiple types and or references (we don't use oneOf and allOf in ISA)
     // for loop with a recursive call ?
-  }
-  else if (Object.keys(references).includes('type') && references.type === 'object'){
+  } else if (
+    Object.keys(references).includes("type") &&
+    references.type === "object"
+  ) {
     // this is a nested reference aka a reference to an object not contained in its own schema
     // we need to create a new schema for that field that will end up in a separate file.
   }
-  return {[assocProp[0]]: relationTemplate(to_many, schemaName)}
-}
+  const foreignKeyName = to_many ? "_ids" : "_id";
+  return [
+    { [assocProp[0]]: relationTemplate(to_many, schemaName) },
+    foreignKeyName,
+  ];
+};
 
-const processScalar = function(scalarProp) {
+const processScalar = function (scalarProp) {
   console.log(`Processing scalar ${scalarProp[0]}`);
   if (scalarProp[0] !== "@id") {
-    const result = { [scalarProp[0]]: recognizeIsaType(scalarProp[1]) }
-    console.log(result)
-    return result
+    const result = { [scalarProp[0]]: recognizeIsaType(scalarProp[1]) };
+    console.log(result);
+    return result;
   }
-}
+};
 
-function getSchemaName(string) { return string.replace('_schema.json', '').replace("#", '')}
+function getSchemaName(string) {
+  return string.replace("_schema.json", "").replace("#", "");
+}
 
 function relationTemplate(toMany, schemaName) {
   return {
-    type: toMany ? 'to_many' : 'to_one',
+    type: toMany ? "to_many" : "to_one",
     target: schemaName,
     targetStorageType: "sql",
     targetKey: null,
     keyIn: null,
-    label: null
-  }
+  };
 }
 
 module.exports = {
@@ -98,5 +118,5 @@ module.exports = {
   processAssociation,
   processScalar,
   getSchemaName,
-  relationMapping
-}
+  relationMapping,
+};

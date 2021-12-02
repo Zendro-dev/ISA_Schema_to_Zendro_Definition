06799af317301772ec4a309ff1764bba5bc67daa
Dr. Asis Hallab <asis.hallab@gmail.com>
2021-12-02 11:19:39 -0600

    Implemented habndling of 'anyOf' associations

--- utils.js
+++ utils.js
@@ -7,16 +7,17 @@ const mapIsaTypeToZendroType = {
   email: "String",
   uri: "String",
 };
+let zendroAttributes = {
+  model: PARENT,
+  storageType: "sql",
+  attributes: {},
+  associations: {},
+};
 
 const processProperties = function (propertiesObj, parentName) {
   PARENT = getSchemaName(parentName);
+  zendroAttributes.model = PARENT;
   const props = Object.entries(propertiesObj);
-  let zendroAttributes = {
-    model: PARENT,
-    storageType: "sql",
-    attributes: {},
-    associations: {},
-  };
   props.forEach((prop) => {
     const propDef = prop[1];
     if (propDef.type === "array") {
@@ -63,7 +64,7 @@ const processArray = function (arrayProp) {
 
 const processAssociation = function (assocProp) {
   console.log(`Processing association ${JSON.stringify(assocProp[0])}`);
-  const to_many = "items" in assocProp[1],
+  let to_many = "items" in assocProp[1],
     references = "items" in assocProp[1] ? assocProp[1].items : assocProp[1],
     schemaName = "";
   if (Object.keys(references).includes("$ref")) {
@@ -73,8 +74,40 @@ const processAssociation = function (assocProp) {
       ? relationMapping[schemaName].push(PARENT)
       : (relationMapping[schemaName] = [PARENT]);
   } else if (Object.keys(references).includes("anyOf")) {
+    const no_refs = references.anyOf.every(element => Object.keys(element)[0] !== "$ref");
+    console.log(`\n\nanyOf case for field '${assocProp[0]}' to_many? '${to_many}' no_refs? ${no_refs}:\n${JSON.stringify(references)}\n\n`);
+    if (no_refs) {
+      const attrType = to_many ? "[String]" : "String"
+      zendroAttributes.attributes[assocProp[0]] = attrType;
+    } else {
+      const refs = references.anyOf.filter(element => Object.keys(element)[0] === "$ref");
+      let ref_assocs = refs.map(r => {
+        const target = getSchemaName(r["$ref"]);
+        const assocName = `${assocProp[0]}_${target}`;
+        const targetKey = `${target}_id`;
+        const keyIn = to_many ? zendroAttributes.model : target;
+        console.error(`keyIn: ${keyIn}`);
+        const assocDef = relationTemplate(to_many, target, targetKey, keyIn);
+        zendroAttributes.associations[assocName] = assocDef;
+        console.error(`anyOf - assoc '${assocName}':\n` + JSON.stringify(zendroAttributes.associations[assocName]));
+        // Add foreign-key if on the to-one end:
+        if (to_many) {
+          zendroAttributes.attributes[targetKey] = 'String';
+          //console.error("anyOf - FK:\n" + JSON.stringify(zendroAttributes.attributes));
+        }
+      })
+    }
+    // assocPorps = {}
+    // How do I know, whether this is type array or not
+    //if (to_many) {} else {}
+    // skip non $ref, except when there is no $ref use a string
     // there are multiple types and or references (we don't use oneOf and allOf in ISA)
     // for loop with a recursive call ?
+    // For each entry in references we need one association, except no $ref
+    //return [
+    //  { [assocProp[0]]: relationTemplate(to_many, schemaName) },
+    //  foreignKeyName,
+    //];
   } else if (
     Object.keys(references).includes("type") &&
     references.type === "object"
@@ -102,13 +135,15 @@ function getSchemaName(string) {
   return string.replace("_schema.json", "").replace("#", "");
 }
 
-function relationTemplate(toMany, schemaName) {
+function relationTemplate(toMany, schemaName, targetKey, keysIn) {
   return {
     type: toMany ? "to_many" : "to_one",
     target: schemaName,
     targetStorageType: "sql",
-    targetKey: null,
-    keyIn: null,
+    implementation: "foreignkeys",
+    reverseAssociation: "TO-DO",
+    targetKey,
+    keysIn
   };
 }
 
